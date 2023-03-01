e5e2a424005d9d64937712947ad872120dc648e4
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2023-03-01 14:16:12 +0100

    JS password matching, if so green border and red if not. Smaller design changes: edited text and width of paragraph

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var input = document.querySelector("#phone");
+let input = document.querySelector("#phone");
 window.intlTelInput(input, {
     // show dial codes too
     separateDialCode: true,
@@ -8,3 +8,21 @@ window.intlTelInput(input, {
     //Default country
     initialCountry:"us",
 });
+
+    
+    var check = function() {
+        let pass1 = document.getElementById('password');
+        let pass2 = document.getElementById('confirmPass');  
+        if (pass1.value == pass2.value) {
+            pass1.style.borderBottom = '2px solid #77c700';
+            pass1.style.borderRight = '2px solid #77c700';
+            pass2.style.borderBottom = '2px solid #77c700';
+            pass2.style.borderRight = '2px solid #77c700';
+        } else {
+            pass1.style.borderBottom = '2px solid red';
+            pass1.style.borderRight = '2px solid red';
+            pass2.style.borderBottom = '2px solid red';
+            pass2.style.borderRight = '2px solid red';
+        }
+    }
+
